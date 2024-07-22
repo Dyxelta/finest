f1fff52cb72c6576919b5496b18a6fa54eeb740f
Dixon <dixonwillow23@gmail.com>
2024-07-22 20:54:24 +0700

    feat: show category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function showAllCategories() {
+        $expenseCategories = Category::where('category_is_income', false)->get();
+        $incomeCategories = Category::where('category_is_income', true)->get();
+
+        return [
+            'expenseCategories' => $expenseCategories,
+            'incomeCategories' => $incomeCategories
+        ];
+    }
 }
