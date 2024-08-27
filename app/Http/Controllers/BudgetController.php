e5b29d76769d9c630373c518aece6b58cdb1c170
Dixon <dixonwillow23@gmail.com>
2024-08-27 21:56:30 +0700

    fix: complete data sent for budget

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
-    public function addBudget(Request $request) {
+    public function addBudget(Request $request)
+    {
         $request->validate([
             'budget_name' => 'required|unique:budgets',
             'budget_amount' => 'required|numeric',
@@ -30,14 +31,16 @@ class BudgetController extends Controller
         return redirect()->back();
     }
 
-    public function deleteBudget(Request $request) {
+    public function deleteBudget(Request $request)
+    {
         $budget = Budget::findOrFail($request->id);
 
         $budget->delete();
         return redirect()->back();
     }
 
-    public function editBudget(Request $request) {
+    public function editBudget(Request $request)
+    {
         $request->validate([
             'budget_name' => 'required',
             'budget_amount' => 'required|numeric',
@@ -63,29 +66,38 @@ class BudgetController extends Controller
         return redirect()->back();
     }
 
-    public function showBudgetById($id) {
+    public function showBudgetById($id)
+    {
         $user = auth()->user();
         $budget = Budget::firstWhere('user_id', 1)->where('id', $id)->get();
 
         return ['budget' => $budget];
     }
 
-    public function showAllUserBudget() {
+    public function showAllUserBudget()
+    {
         $user = auth()->user();
-        $budgets = Budget::where('user_id', $user->id)->with('category')->get();
+        $budgets = Budget::where('user_id', $user->id)
+            ->with(['category' => function ($query) use ($user) {
+                $query->with(['transactions' => function ($query) use ($user) {
+                    $query->where('user_id', $user->id);
+                }]);
+            }])
+            ->get();
 
         return ['budgets' => $budgets];
     }
 
-    public function showReccommendedDailyExpense() {
+    public function showReccommendedDailyExpense()
+    {
         $user = auth()->user();
         $budget = Budget::firstWhere('user_id', $user->id)->get()->first()->budget_amount;
         $year = date('Y');
         $month = date('m');
         $day = cal_days_in_month(CAL_GREGORIAN, $month, $year);
 
-        $rec_budget = $budget/$day;
-        $rec_budget = (int)floor($rec_budget/1000)*1000;
+        $rec_budget = $budget / $day;
+        $rec_budget = (int)floor($rec_budget / 1000) * 1000;
         return ['budget' => number_format($rec_budget)];
     }
 }
