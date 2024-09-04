52f618258536ca06b2f48e7d2cc4ec883c0133d3
Dixon <dixonwillow23@gmail.com>
2024-09-04 21:58:24 +0700

    feat: transaction by category report send to front end

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -230,7 +230,7 @@ class TransactionController extends Controller
             ->orderBy('month', 'desc')
             ->get();
 
-            $summaryReport = Transaction::selectRaw('SUM(transaction_amount) as total_amount, categories.category_is_income')
+        $summaryReport = Transaction::selectRaw('SUM(transaction_amount) as total_amount, categories.category_is_income')
             ->join('categories', 'transactions.category_id', '=', 'categories.id')
             ->where('transactions.user_id', $userId)
             ->where('transactions.wallet_id', $request->walletId)
@@ -243,12 +243,12 @@ class TransactionController extends Controller
                 ];
             });
 
-            $summaryReport = $summaryReport->merge([
-                'income' => $summaryReport->get('income',0),
-                'expense' => $summaryReport->get('expense',0)
-            ]);
+        $summaryReport = $summaryReport->merge([
+            'income' => $summaryReport->get('income', 0),
+            'expense' => $summaryReport->get('expense', 0)
+        ]);
 
-            $summaryReport['total_balance'] = $summaryReport['income'] - $summaryReport['expense'];
+        $summaryReport['total_balance'] = $summaryReport['income'] - $summaryReport['expense'];
 
         return [
             'monthly_expense_data' => $expenseDataPerMonth,
