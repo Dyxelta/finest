94a9e65ab162bccc3e26c561664d1f5a3b435ff4
kllerqueen <102903801+kllerqueen@users.noreply.github.com>
2024-09-25 20:55:34 +0700

    fix: register password regex

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,7 @@ class UserController extends Controller
                 'regex:/[a-z]/',
                 'regex:/[A-Z]/',
                 'regex:/[0-9]/',
-                'regex:/[!@#$%^&*(),.?":{}|<>_\-+=~`\/[\]\\]/',
+                'regex:/[!@#$%^&*(),.?":{}|<>_\-+=~`\/[\]\\\\]/',
             ],
             'confirm_pass' => 'required_with:password|same:password',
         ]);
