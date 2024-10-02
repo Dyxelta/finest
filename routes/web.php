334fda00d18346d3ea2d698f626e0764a5eba2f3
Dixon <dixonwillow23@gmail.com>
2024-10-02 20:15:51 +0700

    feat: send wallet total balance data to dashboard

--- routes/web.php
+++ routes/web.php
@@ -51,13 +51,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', function (Request $request) {
         $transactionData = app(TransactionController::class)->showAllUserTransaction($request);
 
+        $summaryReportData = app(TransactionController::class)->showSummaryReportData($request);
+
         $walletData = app(WalletController::class)->showAllWalletByUserID();
+
+        $totalWalletBalance = app(WalletController::class)->showUserWalletTotalBalance();
 
         $budgetData = app(BudgetController::class)->showAllUserBudget($request);
 
-        $summaryReportData = app(TransactionController::class)->showSummaryReportData($request);
-
-        return Inertia::render('Dashboard', array_merge($transactionData, $walletData, $budgetData, $summaryReportData));
+        return Inertia::render('Dashboard', array_merge($transactionData, $summaryReportData, $walletData, $totalWalletBalance, $budgetData));
     })->name('dashboard');
 
 
