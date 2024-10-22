fdd23667313c6b2dac4c063c2f41d370ccd5814f
wete <williamtan1232@gmail.com>
2024-10-22 22:08:19 +0700

    fix: fix post route verification.send

--- resources/js/Helpers/utils.jsx
+++ resources/js/Helpers/utils.jsx
@@ -1,5 +1,4 @@
 import CustomShowAlertModal from "@/Components/Modal/CustomFunctionAlertModal";
-import React, { useEffect } from "react";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 
